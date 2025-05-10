99b5c6eb23b7468b12aa6dcd46d3c29db2050544
Sean Caleb <scmubias@gmail.com>
2025-05-10 09:15:40 +0800

    update changes

--- assets/custom-faq.js
+++ assets/custom-faq.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
-  $('.collapsible__title-wrapper').click(function () {
-    const $item = $(this).closest('.collapsible__item');
+  $('.faq__title-wrapper').click(function () {
+    const $item = $(this).closest('.faq__item');
     const $caret = $(this).find('svg');
 
     if ($item.hasClass('active')) {
-      $item.removeClass('active').find('.collapsible__text').slideUp(150);
+      $item.removeClass('active').find('.faq__text').slideUp(150);
       $caret.removeClass('rotated');
     } else {
-      $('.collapsible__item').removeClass('active').find('.collapsible__text').slideUp(150);
-      $('.collapsible__item svg').removeClass('rotated');
+      $('.faq__item').removeClass('active').find('.faq__text').slideUp(150);
+      $('.faq__item svg').removeClass('rotated');
 
-      $item.addClass('active').find('.collapsible__text').slideDown(150);
+      $item.addClass('active').find('.faq__text').slideDown(150);
       $caret.addClass('rotated');
     }
   });
